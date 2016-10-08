cb8a4b6d8f25b1b189bd0a8e763ec1169581b7b2
Kurt McCann <kurtmccann@MacBook-Pro-20.local>
2016-10-08 10:57:33 -0400

    Coplete Part 2: add Pet class and subclasses

--- js/script.js
+++ js/script.js
@@ -17,14 +17,63 @@ function PetChat(){
     }
   };
 
-  this.init = function(){
-    while(numPets === null){
+  this.init = function()
+  {
+    while(numPets === null)
+    {
       numPets = askHowMany();
     }
 
     askPetInfo();
   }
 };
+
+function Pet(petName)
+{
+  this.petName = petName;
+  setName = function(newPetName)
+  {
+    this.newPetName = n;
+  }
+  getName = function()
+  {
+    return this.petName;
+  }
+  speak = function()
+  {
+    display.innerHTML("");
+  }
+}
+
+function Cat(name)
+{
+  Pet.call(this, name);
+  speak = function()
+  {
+    display.innerHTML("Meow");
+  }
+}
+Cat.prototype = new Pet;
+
+function Dog()
+{
+  Pet.call(this, name);
+  this.speak = function()
+  {
+    display.innerHTML("Woof");
+  }
+}
+Dog.prototype = new Pet;
+
+function Bird()
+{
+  Pet.call(this, name);
+  this.speak = function()
+  {
+    display.innerHTML("Caw")
+  }
+}
+Bird.prototype = new Pet;
 
 var chat = new PetChat();
 chat.init();
